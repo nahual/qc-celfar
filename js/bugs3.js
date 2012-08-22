88f0edc6c304a965acb51c512d1bb9ef1e61e5ff
Mariano Simone <marianosimone@gmail.com>
2012-08-22 15:14:35 -0300

    agregando bugs en la v3

---
+++ js/bugs3.js
@@ -0,0 +1,18 @@
+converters.celcius = function(celcius) {
+    if (celcius == 0) return 0;
+    if (celcius < 0) return ((parseFloat(celcius)*9.0)/5.0); 
+    return ((parseFloat(celcius)*9.0)/5.0) + 32;
+}
+
+validators = [
+    {"valid" : function(input) {
+        if (input.indexOf(',') != -1) {
+            alert("Valor inesperado");
+            return false;
+        } else {
+            return true;
+        }
+      },
+      "error" : "" }
+].concat(validators);
+
