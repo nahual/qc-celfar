81713d904f065200055bb0bb63d0b7bd83784f1e
Mariano Simone <marianosimone@gmail.com>
2012-09-04 23:29:07 -0300

    Corrigiendo meta-bug que hacía que la v2 fuese a la v5 cuando se dejaba el campo vacío~

--- js/bugs2.js
+++ js/bugs2.js
@@ -1,4 +1,12 @@
 $("p#description")[0].innerHTML = "Conversor de grados <a href='http://es.wikipedia.org/iki/Grado_Celsius'>Celcius</a> y <a href='http://es.wikipedia.org/wiki/Grado_Fahrenheit'>Fahrenheit</a>";
 
 validators[VAL_ZERO].valid = function(input) {return parseInt(input) > -273;};
-validators = [{"valid" : function(input) {if (input.length == 0) {exit(1)} return true;}}].concat(validators);
+validators = [
+    {"valid" : function(input) {
+        if (input.length == 0) {
+            return false;
+        }
+        return true;
+      },
+     "error" : "" }
+].concat(validators);
